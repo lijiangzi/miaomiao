b1fb14f65adc4d0242f18d50d310313be6092d06
lijiangzi <2206208402@qq.com>
2019-05-18 01:01:11 +0800

    该分支完成了喵喵的静态路由布局

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="main">
+        <Header></Header>
+        <div id="content">
+            <login></login>
+        </div>
+        <TabBar></TabBar>
+    </div>
+</template>
+<script>
+import Header from "@/components/Header"
+import TabBar from "@/components/TabBar"
+import Login from "@/components/Login"
+export default {
+    name: "Mine",
+    components:{
+        Header,
+        TabBar,
+        Login
+    }
+}
+</script>
